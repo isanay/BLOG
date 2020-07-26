b9c21da69308b0fd90c23ec09b60187e9e147cfb
ISanay <ssanay@ucacue.edu.ec>
2020-07-25 22:47:06 -0500

    version 3

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
+  belongs_to :user
+  validate_presence_of :post_id
+  validate_presence_of :body
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  validate_presence_of :title
+  validate_presence_of :body
+  belongs_to :user
 end
